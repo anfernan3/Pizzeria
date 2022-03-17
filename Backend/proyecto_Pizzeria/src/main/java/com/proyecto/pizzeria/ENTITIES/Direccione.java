069963272d65f7a16d645852d32b42ce83c90fa0
jradeon <juan-antonio.espin-fernandez@capgemini.com>
2022-03-17 13:40:36 +0100

    cambios

---
+++ Backend/proyecto_Pizzeria/src/main/java/com/proyecto/pizzeria/ENTITIES/Direccione.java
@@ -0,0 +1,78 @@
+package com.proyecto.pizzeria.ENTITIES;
+
+
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the direcciones database table.
+ * 
+ */
+@Entity
+@Table(name="direcciones")
+@NamedQuery(name="Direccione.findAll", query="SELECT d FROM Direccione d")
+public class Direccione implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_direccion")
+	private int idDireccion;
+
+	private String calle;
+
+	private String ciudad;
+
+	private String pais;
+
+	//bi-directional many-to-one association to Usuario
+	@ManyToOne
+	@JoinColumn(name="id_usuario")
+	private Usuario usuario;
+
+	public Direccione() {
+	}
+
+	public int getIdDireccion() {
+		return this.idDireccion;
+	}
+
+	public void setIdDireccion(int idDireccion) {
+		this.idDireccion = idDireccion;
+	}
+
+	public String getCalle() {
+		return this.calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public String getCiudad() {
+		return this.ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getPais() {
+		return this.pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
--- Backend/proyecto_Pizzeria/src/main/java/com/proyecto/pizzeria/REPOSITORIES/IngredientesRepository.java
+++ Backend/proyecto_Pizzeria/src/main/java/com/proyecto/pizzeria/REPOSITORIES/IngredientesRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import com.proyecto.pizzeria.ENTITIES.Ingredientes;
+
+import com.proyecto.pizzeria.ENTITIES.Ingrediente;
+
 
 @RepositoryRestResource(exported = false)
-public interface IngredientesRepository extends JpaRepository<Ingredientes, Integer> {
+public interface IngredientesRepository extends JpaRepository<Ingrediente, Integer> {
 	<T> List<T> findByIngredientesPizzaIdIsNotNull(Class<T> type);
 	<T> Iterable<T> findByIngredientesPizzaIdIsNotNull(Sort sort, Class<T> type);
 	<T> Page<T> findByIngredientesPizzaIdIsNotNull(Pageable pageable, Class<T> type);
