9a2ff1391ef84fd6c7905939367acdbea8d73863
miguelZM23 <miguel.zapata-martinez@capgemini.com>
2022-03-30 01:10:46 +0200

    habilitado eureka en los servicios

--- Backend/ms.autentication/src/main/java/com/capgemini/Application.java
+++ Backend/ms.autentication/src/main/java/com/capgemini/Application.java
@@ -3,12 +3,14 @@ package com.capgemini;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application implements CommandLineRunner{
 
