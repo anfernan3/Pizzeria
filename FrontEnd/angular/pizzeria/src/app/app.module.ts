177d4bb2ce78588b87ee3266676481d8010a159e
miguelZM23 <miguel.zapata-martinez@capgemini.com>
2022-03-30 02:27:18 +0200

    componente angular fotos chef

--- FrontEnd/angular/pizzeria/src/app/app.module.ts
+++ FrontEnd/angular/pizzeria/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChefComponent } from './chef/chef.component';
+import {ChefModule} from './chef/chef.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, ChefModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
