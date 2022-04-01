796300364aac82b42d7bc9a2cafa40aa674300bd
afernan3 <antonio.fernandez-tomas@capgemni.com>
2022-04-01 15:09:11 +0200

    menu y registro completo con estilos

--- FrontEnd/angular/pizzeria/src/app/security/security.module.ts
+++ FrontEnd/angular/pizzeria/src/app/security/security.module.ts
@@ -5,6 +5,8 @@ import { RegisterUserComponent } from './register-user/register-user.component';
 
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
 
 const routes: Routes = [
   { path: 'registro', component: RegisterUserComponent },
@@ -13,6 +15,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule, FormsModule, ReactiveFormsModule, RouterModule.forChild(routes),
+    PasswordModule, ButtonModule
   ],
   declarations: [ LoginComponent, RegisterUserComponent ],
   exports: [ LoginComponent, RegisterUserComponent ],
