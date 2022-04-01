ef742c2d665c0707d00e30cb4f233e427650ba2f
jradeon <juan-antonio.espin-fernandez@capgemini.com>
2022-04-01 11:09:53 +0200

    servicio ingredientes completo y funcionando a tope

--- Backend/proyecto_Pizzeria/src/main/java/com/proyecto/pizzeria/controllers/PedidosControllers.java
+++ Backend/proyecto_Pizzeria/src/main/java/com/proyecto/pizzeria/controllers/PedidosControllers.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -36,7 +37,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/pedidos")
--- FrontEnd/angular/pizzeria/src/app/ingredientes/modulo.module.ts
+++ FrontEnd/angular/pizzeria/src/app/ingredientes/modulo.module.ts
@@ -4,31 +4,39 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { MyCoreModule } from 'src/lib/my-core';
 import { CommonServicesModule } from '../common-services';
-import { IngredientesAddComponent, IngredientesComponent, IngredientesEditComponent, IngredientesListComponent, IngredientesViewComponent, INGREDIENTES_COMPONENTES } from './componente.component';
-import {PaginatorModule} from 'primeng/paginator';
+import {
+  IngredientesAddComponent,
+  IngredientesEditComponent,
+  IngredientesListComponent,
+  IngredientesViewComponent,
+  INGREDIENTES_COMPONENTES,
+} from './componente.component';
+import { PaginatorModule } from 'primeng/paginator';
 import { CommonComponentModule } from '../common-component';
 
-
 const routes: Routes = [
-   { path: '', component:  IngredientesListComponent },
-    { path: 'add', component:  IngredientesAddComponent },
-    { path: ':id/edit', component:  IngredientesEditComponent },
-    { path: ':id', component:  IngredientesViewComponent },
-     { path: ':id/:kk', component:  IngredientesViewComponent },
-]
+  { path: '', component: IngredientesListComponent },
+  { path: 'add', component: IngredientesAddComponent },
+  { path: ':id/edit', component: IngredientesEditComponent },
+  { path: ':id', component: IngredientesViewComponent },
+  { path: ':id/:kk', component: IngredientesViewComponent },
+];
 
 @NgModule({
-  declarations: [
-    INGREDIENTES_COMPONENTES,
-  ],
+  declarations: [INGREDIENTES_COMPONENTES],
   exports: [
     INGREDIENTES_COMPONENTES,
-    // IngredientesComponent,
+
   ],
   imports: [
-    CommonModule, FormsModule, RouterModule.forChild(routes),
-    MyCoreModule, CommonServicesModule,
-    PaginatorModule, CommonComponentModule, MyCoreModule,
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+    MyCoreModule,
+    CommonServicesModule,
+    PaginatorModule,
+    CommonComponentModule,
+    MyCoreModule,
+  ],
 })
-export class IngredientesModule { }
+export class IngredientesModule {}
